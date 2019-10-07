7b678e02236a1de1077c4d4b342f86b77b047756
Inácio Ferraz <zoawbrc@gmail.com>
2019-10-07 10:04:06 -0300

    Começando o curso da HCODE

---
+++ js/index.js
@@ -0,0 +1,145 @@
+// let a = 10;
+// const b = "10";
+
+// console.log(a === b);
+// console.log(typeof b == "string")
+
+// let cor = "verde";
+
+// if (cor === "verde") {
+//     console.log("siga");
+// } else {
+//     console.log("pare")
+// }
+
+
+// switch (cor) {
+
+//     case "verde":
+//         console.log("siga");
+//         break;
+
+//     case "verde":
+//         console.log("pare");
+//         break;
+
+//     case "verde":
+//         console.log("PARA, POHA");
+//         break;
+
+//     default:
+//         console.log("TO CEGOOOO");
+//         break;
+// }
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let n = 7;
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`${i} X ${n} = ${i*n}`);
+// }
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// function calc(a, b, operator) {
+
+//     return eval(`${a} ${operator} ${b}`);
+
+// }
+
+// let resultado = calc(10, 10, "+");
+
+// console.log(resultado);
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// (function(a, b, operator) {
+
+//     return eval(`${a} ${operator} ${b}`);
+
+// })(10, 10, "+");
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// let calc = (a, b, operator) => {
+//     return eval(`${a} ${operator} ${b}`);
+
+// }
+
+// let resultado = calc(10, 10, "+");
+
+// console.log(resultado);
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let calc = (a, b, operator) => {
+//     return eval(`${a} ${operator} ${b}`);
+
+// }
+
+// let resultado = calc(10, 10, "+");
+
+// console.log(resultado);
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+// window.addEventListener("focus", event => {
+//     console.log("FOCUS");
+// })
+
+// document.addEventListener("click", event => {
+//     console.log("click");
+// })
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+// let agora = new Date();
+
+
+// console.log(agora.toLocaleDateString("pt-BR"));
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+// let carros = ["palio 98", "toro", "uno"];
+// carros.forEach(function(value, index) {
+//         console.log(value, index)
+//     })
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+// let celular = function() {
+//     this.cor = "prata";
+
+//     this.ligar = function() {
+//         console.log("Uma ligação");
+//         return "ligando"
+//     }
+// }
+
+// let objeto = new celular();
+
+// console.log(objeto.ligar());
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+// class celular {
+
+//     constructor() {
+//         this.cor = "prata";
+//     }
+
+//     ligar() {
+//         console.log("Uma ligação");
+//         return "ligando"
+//     }
+// }
+
+// let objeto = new celular();
+
+// console.log(objeto.ligar());
+//=-=-=-=-=-=-=-=--==-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
